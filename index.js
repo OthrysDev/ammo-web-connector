83ebc4871415624b1d6eb75ac36e417f3d45c47b
Axel <axeletcana@hotmail.fr>
2021-02-08 21:51:46 +0100

    feat: Initial commit

--- index.js
+++ index.js
@@ -1,132 +1,73 @@
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener('fetch', (event) => {
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    function serialize(request) {
-        const headers = {};
-        // eslint-disable-next-line no-restricted-syntax
-        for (const entry of request.headers.entries()) {
-            // eslint-disable-next-line prefer-destructuring
-            headers[entry[0]] = entry[1];
-        }
+async function Connector(event) {
+  serialize(event.request).then((serializedRequest) => {
+    if (!url.includes('socket')) {
+      fetch(url, {
+        method,
+        headers: reqHeaders,
+        body: serializedRequest.body,
+      })
+        .then(async (r) => {
+          for (const [header, value] of r.headers) {
+            resHeaders[header] = value;
+          }
 
-        const serialized = {
-            url: request.url,
-            headers,
-            method: request.method,
-            mode: request.mode,
-            credentials: request.credentials,
-            cache: request.cache,
-            redirect: request.redirect,
-            referrer: request.referrer,
-        };
-        if (request.method !== 'GET' && request.method !== 'HEAD') {
-            return request
-                .clone()
-                .text()
-                .then((body) => {
-                    serialized.body = body;
-                    return Promise.resolve(serialized);
-                });
-        }
-        return Promise.resolve(serialized);
-    }
+          response.status = r.status;
 
-    const { headers, method, url } = event.request;
+          if (r.ok) {
+            return r.json();
+          }
 
-    const reqHeaders = {};
-    const resHeaders = {};
+          const reader = r.body.getReader();
 
-    const response = {
-        headers: resHeaders,
-    };
+          const encodedResponse = await reader.read();
 
-    // eslint-disable-next-line no-restricted-syntax
-    for (const [header, value] of headers) {
-        reqHeaders[header] = value;
-    }
-
-    if (event.request.url.includes('pokemon')) {
-        console.log('sw.js line 4 ----> event', event.request);
-        console.log('sw.js line 8 ----> reqHeaders', reqHeaders);
-    }
-
-    serialize(event.request).then((serializedRequest) => {
-        if (event.request.url.includes('pokemon')) {
-            console.log('sw.js line 47 ----> url', url);
-        }
-
-        if (!url.includes('socket')) {
-            fetch(url, {
-                method,
+          const stringifiedValue = new TextDecoder('utf-8').decode(
+            encodedResponse.value
+          );
+          response.body = stringifiedValue;
+        })
+        .then((res) => {
+          response.body = res;
+        })
+        .catch((err) => {
+          console.error(
+            'An error happened while trying to execute the fetch request : ',
+            err
+          );
+        })
+        .finally(() => {
+          const data = {
+            data: {
+              method,
+              url,
+              request: {
                 headers: reqHeaders,
                 body: serializedRequest.body,
-            })
-                .then(async (r) => {
-                    if (event.request.url.includes('pokemon')) {
-                        console.log('sw.js line 55 ----> r', r);
-                    }
+              },
+              response,
+            },
+          };
 
-                    // eslint-disable-next-line no-restricted-syntax
-                    for (const [header, value] of r.headers) {
-                        resHeaders[header] = value;
-                    }
+          const options = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+          };
 
-                    response.status = r.status;
+          fetch('http://localhost:3001', options);
+        });
+    }
+  });
+}
 
-                    if (r.ok) {
-                        return r.json();
-                    }
-
-                    const reader = r.body.getReader();
-
-                    const encodedResponse = await reader.read();
-
-                    const stringifiedValue = new TextDecoder('utf-8').decode(
-                        encodedResponse.value
-                    );
-                    response.body = stringifiedValue;
-                })
-                .then((res) => {
-                    response.body = res;
-                    console.log('sw.js line 64 ----> Went well', res);
-                })
-                .catch((err) => {
-                    if (event.request.url.includes('pokemon')) {
-                        console.log('sw.js line 89 ----> Went bad', err);
-                    }
-                })
-                .finally(() => {
-                    const data = {
-                        data: {
-                            method,
-                            url,
-                            request: {
-                                headers: reqHeaders,
-                                body: serializedRequest.body,
-                            },
-                            response,
-                        },
-                    };
-
-                    const options = {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(data), // body data type must match "Content-Type" header
-                    };
-
-                    fetch('http://localhost:3001', options)
-                        .then((r) => r.json())
-                        .then((resp) => {
-                            if (event.request.url.includes('pokemon')) {
-                                console.log(
-                                    'sw.js line 84 ----> Back end answered',
-                                    resp
-                                );
-                            }
-                        });
-                });
-        }
-    });
+self.addEventListener('fetch', (event) => {
+  Connector(event);
 });
+
+
+self.addEventListener('image', (event) => {
+    Connector(event);
+  });
+  
