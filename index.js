e2dd24476cbe8b1478529b1e772adf5f820091d0
Axel <axeletcana@hotmail.fr>
2021-02-23 15:46:54 +0100

    feat: As it is impossible to refactor in a module, did the correction left on the comments

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-async function Connector(event) {
+/* eslint-disable no-restricted-syntax */
+async function connector(event) {
+  const AMMO_ADDRESS = 'http://localhost:2001';
+
   function extractHeadersValues(headers) {
+    // The request's headers are not a valid JS object, we need to apply a special treatment to recover the values.
     const extractedHeaders = {};
 
+    // A for.. of method is the only one available. Hearders is not an array, therefore can't use array iteration methods.
     for (const [header, value] of headers) {
       extractedHeaders[header] = value;
     }
@@ -10,7 +15,6 @@ async function Connector(event) {
   }
 
   function serialize(request) {
-    // To be able to read the request as a valid JS object, we need this function to extract everything
     const headers = extractHeadersValues(request.headers.entries());
 
     const serialized = {
@@ -24,23 +28,27 @@ async function Connector(event) {
       referrer: request.referrer,
     };
 
-    // TODO Explain this part, maybe link the stackoverflow ?
-    if (request.method !== 'GET' && request.method !== 'HEAD') {
-      return request
-        .clone()
-        .text()
-        .then((body) => {
-          serialized.body = body;
-          return Promise.resolve(serialized);
-        });
-    }
-    return Promise.resolve(serialized);
+    /* 
+          https://stackoverflow.com/questions/43813770/how-to-intercept-all-http-requests-including-form-submits/43815800
+          To be able to read the potential body from a request, we need to clone the request itself.
+  
+          In addition, a GET or HEAD request is not supposed to have any body, but it can still happen. We simply check if there's a body,
+          no matter what kind of requests this is.
+      */
+    return request
+      .clone()
+      .text()
+      .then((body) => {
+        if (body) serialized.body = body;
+
+        return Promise.resolve(serialized);
+      });
   }
 
   // Recover the necessary informations from the intercepted request
   const { headers, method, url } = event.request;
 
-  // request and response are the necessary informations we need to send to Ammo
+  // request and response are the necessary objects we need to send to Ammo
   const request = {
     headers: {},
     body: null,
@@ -55,82 +63,70 @@ async function Connector(event) {
   request.headers = extractHeadersValues(headers);
 
   serialize(event.request).then((serializedRequest) => {
-    // For now, we exclude the websocket protocol
-    // TODO Handle me differently as a legit url can contain the word socket
-    if (!url.includes('socket')) {
-      request.body = serializedRequest;
+    request.body = serializedRequest;
 
-      /*
-        We need to execute the intercepted request by ourself to access the response data.
-        TODO : Maybe find another way to handle it, and check for performance issue
-      */
-      fetch(url, {
-        method,
-        headers: request.headers,
-        body: serializedRequest.body,
-      })
-        .then(async (r) => {
-          // Now that the response object is here, fill the object with the necessary informations
-          response.headers = extractHeadersValues(r.headers);
-          response.status = r.status;
+    fetch(url, {
+      method,
+      headers: request.headers,
+      body: serializedRequest.body,
+    })
+      .then(async (r) => {
+        // Now that the response object is available, fill the object with the necessary informations
+        response.headers = extractHeadersValues(r.headers);
+        response.status = r.status;
 
-          // If the response say "Alright it's all good", return it as usual
-          if (r.ok) {
-            return r.json();
-          }
+        // If the response say "Alright it's all good", stop the treatment
+        if (r.ok) {
+          response.body = await r.json();
+          return;
+        }
 
-          /*
-           But sometimes, the body is returned as a Stream. In this case, we need to read it,
-           decode the value recovered and finally save the final value into the response object.
+        /*
+              But sometimes, the body is returned as a Stream. In this case, we need to read it,
+              decode the value recovered and finally save the final value into the response object.
            */
-          const reader = r.body.getReader();
+        const reader = r.body.getReader();
 
-          const encodedResponse = await reader.read();
+        const encodedResponse = await reader.read();
 
-          const stringifiedValue = new TextDecoder('utf-8').decode(
-            encodedResponse.value
-          );
+        const stringifiedValue = new TextDecoder('utf-8').decode(
+          encodedResponse.value
+        );
 
-          response.body = stringifiedValue;
-        })
-        .then((res) => {
-          //! Might not be necessary, could do it directly after the r.json() above
-          response.body = res;
-        })
-        .catch((err) => {
-          // TODO : Do something with the error
-        })
-        .finally(() => {
-          // Once we recovered everything, we actually build the request and send the data to Ammo
-          const data = {
-            data: {
-              method,
-              url,
-              request,
-              response,
-            },
-          };
+        response.body = stringifiedValue;
+      })
+      .catch((err) => {
+        console.error(
+          `An unexpected error happened while executing the actual request, error : ${err.message}`
+        );
+      })
+      .finally(() => {
+        // Once we recovered everything, we actually build the request and send the data to Ammo
+        const data = {
+          data: {
+            method,
+            url,
+            request,
+            response,
+          },
+        };
 
-          const options = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
-          };
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data), // body data type must match "Content-Type" header
+        };
 
-          // TODO : Maybe put into a .env file nor into a well named variable
-          // TODO : Add a .catch to inform the user if something bad happened, aswell as a toggleable log to inform it went well
-          fetch('http://localhost:2001', options);
-        });
-    }
+        //TODO Check the payload too large error on ammo be
+        fetch(AMMO_ADDRESS, options).catch((err) =>
+          console.error(
+            `An error happened while trying to send the data to Ammo, error : ${err.message}`
+          )
+        );
+      });
   });
 }
 
-self.addEventListener('fetch', (event) => {
-  Connector(event);
-});
-
-self.addEventListener('image', (event) => {
-  Connector(event);
-});
+self.addEventListener('fetch', (event) => connector(event));
